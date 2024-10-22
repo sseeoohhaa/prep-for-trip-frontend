31826a957288a02108175a0c7e75e60ad04ade5f
sseeoohhaa <sss121@swu.ac.kr>
2024-10-22 23:14:22 +0900

    feat: Add initial Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Footer.module.css";
+
+function Footer() {
+    return (
+        <div className={styles.footer}>
+           <p>○ Thank you for coming Prep For Trip website. Made in October, 2024 ○</p>
+        </div>
+    );
+}
+
+export default Footer;
