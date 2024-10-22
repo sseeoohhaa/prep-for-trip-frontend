0b3643ba30b0457ac97fdbf4035d483a67969786
sseeoohhaa <sss121@swu.ac.kr>
2024-10-22 23:22:34 +0900

    feat: Add initial canada page

---
+++ src/pages/CanadaPage.js
@@ -0,0 +1,22 @@
+import CityList from "../components/CityList";
+
+// // Code modification required
+const list1 = ["Vancouver", "Victoria", "img", "img"];
+const list2 = ["Kelowna", "Toronto", "img", "img"];
+const list3 = ["Ottawa", "Montreal", "img", "img"];
+const list4 = ["Quebec", "Banff", "img", "img"];
+
+function CanadaPage() {
+    return (
+        <div>
+            <div>
+                <p>캐나다</p>
+                <p>Description 1</p>
+                <p>Description 2</p>
+            </div>
+           
+        </div>
+    );
+}
+
+export default CanadaPage;
