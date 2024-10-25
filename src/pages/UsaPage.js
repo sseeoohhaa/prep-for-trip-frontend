941fe3297c7f8271e274d7fecd7729a28cf340fb
sseeoohhaa <sss121@swu.ac.kr>
2024-10-25 16:50:42 +0900

    feat: Add get city list function by using axios

--- src/pages/UsaPage.js
+++ src/pages/UsaPage.js
@@ -1,11 +1,31 @@
 import CityList from "../components/CityList";
+import axios from "axios";
 
 // // Code modification required
 const list1 = ["Boston", "Newyork", "/img/Boston.jpg", "/img/Newyork.jpg"];
-const list2 = ["Philadelphia", "Newport", "/img/Philadelphia.jpg"];
-const list3 = ["Chicago", "Providence"];
+const list2 = ["Philadelphia", "Newport", "/img/Philadelphia.jpg", "/img/Newport.jpg"];
+const list3 = ["Chicago", "Providence", "/img/Chicago.jpg", "/img/준비중.jpg"];
 
 function UsaPage() {
+
+    const [cityInfo, setCityInfo] = useState(null);
+    const src = "/img/" + cityName + ".jpg";
+    function getCityList() {
+      axios.get("/api/city")
+      .then((res) => {
+        console.log(res);
+        setCityInfo(res.data);
+      }
+      ).catch((err) => {
+        console.log(err);
+      }) // axios
+    }
+    
+    useEffect(() => {
+      getCityList(); // get all city data for map
+    }, []);
+
+
     return (
         <div>
             <div>
@@ -13,9 +33,9 @@ function UsaPage() {
                 <p>Description 1</p>
                 <p>Description 2</p>
 
-                <CityList list={list1} />
-                <CityList list={list2} />
-                <CityList list={list3} />
+                {cityInfo != null ? (
+                    <CityList cityInfo={cityInfo} />
+                ) : null}
             </div>
         </div>
     );
