50a39f148dfa14dcdb8cdf90d9364b9608158480
sseeoohhaa <sss121@swu.ac.kr>
2024-10-25 16:59:20 +0900

    feat: Add get data functions by using axios

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -4,30 +4,67 @@ import styles from '../styles/Detail.module.css';
 import Map from "../components/Map";
 import ModalWindow from "../components//ModalWindow";
 import { Wrapper } from "@googlemaps/react-wrapper";
+import axios from "axios";
 
 const apiKey = process.env.REACT_APP_API_KEY;
-const bostonPageMapLocation = [42.3600825, -71.0588801]; // // Code modification required
 
 function Detail() {
 
-    const cityName = useLocation().pathname.split("/").pop();
+    const cityName = useLocation().pathname.split("/").pop(); // get city name
     const src = "/img/" + cityName + ".jpg";
 
-    const [markerInfo, setMarkerInfo] = useState(null);
-    const [zoom, setZoom] = useState(10);
+    const [zoom, setZoom] = useState(10); // initial map zoom level
+
     const [modalOpen, setModalOpen] = useState(false);
     const [placeInfo, setPlaceInfo] = useState(null);
     function isOpen(state, info) {
         if(state) {
-            setPlaceInfo(info);
             setModalOpen(true);
+            setPlaceInfo(info);
         } else {
             setModalOpen(false);
         }
+    } // modal window open function
+
+    const [markerInfo, setMarkerInfo] = useState(null);
+    const [detailPagePosition, setDetailPagePosition] = useState(null);
+    const [cityInfo, setCityInfo] = useState(null);
+    function getCityInfo() {
+        axios.get("/api/city/"+cityName)
+        .then((res) => {
+          console.log(res);
+          setDetailPagePosition(res.data);
+          setCityInfo(res.data);
+        }
+        ).catch((err) => {
+          console.log(err);
+        }) // axios
     }
 
-    // Code modification required: get all place list When user first access the page
+    function getPlaceList() {
+        axios.get("/api/place/"+cityName)
+        .then((res) => {
+          console.log(res);
+          setMarkerInfo(res.data);
+        }
+        ).catch((err) => {
+          console.log(err);
+        }) // axios
+    }
+
+    function getPlaceInfo() {
+        axios.get("/api/place/"+type)
+        .then((res) => {
+          console.log(res);
+          setMarkerInfo(res.data);
+        }
+        ).catch((err) => {
+          console.log(err);
+        }) // axios
+    }
+
     useEffect(()=> {
+        // Code modification required: get all place list
         const placeMarkerInfoList = [
             {
                 // example
@@ -36,10 +73,14 @@ function Detail() {
                 },
         ];
         setMarkerInfo(placeMarkerInfoList);
+
+        getCityInfo(); // get specific city data
+        //getPlaceList(); // get all place data for map
     },[])
 
     // Code modification required: get all restaurant list when user click place type
-    const clickRestaurantList = () => {
+    function clickRestaurantList() {
+        // getPlaceInfo();
         const placeMarkerInfo = [
             {
                 position: { lat : 42.3513454, lng : -71.0434783 },
@@ -50,6 +91,9 @@ function Detail() {
         setMarkerInfo(placeMarkerInfo);
     }
 
+    function clickHotelList() {}
+    function clicTouristAttraction() {}
+
     return (
         <div>
             {modalOpen ? (
@@ -58,9 +102,12 @@ function Detail() {
             <div>{cityName} 페이지입니다.</div>
             <img alt="city image" src={src} />
             <div className={styles.mapContainer}>
-                <Wrapper apiKey={apiKey} libraries={['marker']}>
-                    <Map position={bostonPageMapLocation} markerInfo={markerInfo} zoom={zoom} page={"detail"} isOpen={isOpen} />
-                </Wrapper>
+                {detailPagePosition != null ? (
+                    <Wrapper apiKey={apiKey} libraries={['marker']}>
+                        <Map position={detailPagePosition} markerInfo={markerInfo} zoom={zoom} page={"detail"} />
+                    </Wrapper>
+                ) : null}
+
                 <div className={styles.placeList}>
                     <div onClick= {() => clickRestaurantList()}>레스토랑</div>
                     <div>호텔</div>
